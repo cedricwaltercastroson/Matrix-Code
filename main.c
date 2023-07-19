1622e9a249ef15fcc2c92a9620caf270ff97ac46
cedricwaltercson <cedricwaltercson@hotmail.com>
2023-07-19 19:55:17 +1000

    updated main.c

--- main.c
+++ main.c
@@ -9,11 +9,6 @@
 #include <SDL_mixer.h>
 #include <SDL_ttf.h>
 
-//
-// TODO: The head is already the right colour or 144, 238, 144. The neck requires a darker green colour and the spacing needs to be modified back to -20 also requires a second random generator also requires modication to the arry on both spawn and move functions *tip explicitly say app.srain[i][1].
-//       The tail is as is however this will require it to have the spacing to be -1000 or -500 and requires the modifcation to the array setup both spawn and move functions *tip explicitly say app.srain[i][2].
-//       int r = (rand() % (max - min + 1)) + min; is a number generator with range
-
 #define EXIT_SUCCESS    0
 #define EXIT_FAILURE    1
 #define RAIN_WIDTH      20
@@ -21,10 +16,9 @@
 #define RAIN_START_Y    0
 #define RAINDROP_COUNT  2048
 #define SpawnFrame      0.01f
-#define MoveFrame       0.075f
-#define RFrame          0.075f
-#define Increment       2 //tail effect remember to also modify incrementmax which is n-1 due to array null terminator
-#define IncrementMax    3 //increment - 1
+#define MoveFrame       0.03f
+#define Increment       3 //tail effect remember to also modify incrementmax which is n-1 due to array null terminator
+#define IncrementMax    4 //increment - 1
 
 void initialize(void);
 void terminate(int exit_code);
@@ -66,6 +60,106 @@ SDL_DisplayMode DM = {
 };
 
 //magic number table for the x coordinator used in random spawning prevents weird overlaps
+
+const int mn[96] = {
+    0,
+    20,
+    40,
+    60,
+    80,
+    100,
+    120,
+    140,
+    160,
+    180,
+    200,
+    220,
+    240,
+    260,
+    280,
+    300,
+    320,
+    340,
+    360,
+    380,
+    400,
+    420,
+    440,
+    460,
+    480,
+    500,
+    520,
+    540,
+    560,
+    580,
+    600,
+    620,
+    640,
+    660,
+    680,
+    700,
+    720,
+    740,
+    760,
+    780,
+    800,
+    820,
+    840,
+    860,
+    880,
+    900,
+    920,
+    940,
+    960,
+    980,
+    1000,
+    1020,
+    1040,
+    1060,
+    1080,
+    1100,
+    1120,
+    1140,
+    1160,
+    1180,
+    1200,
+    1220,
+    1240,
+    1260,
+    1280,
+    1300,
+    1320,
+    1340,
+    1360,
+    1380,
+    1400,
+    1420,
+    1440,
+    1460,
+    1480,
+    1500,
+    1520,
+    1540,
+    1560,
+    1580,
+    1600,
+    1620,
+    1640,
+    1660,
+    1680,
+    1700,
+    1720,
+    1740,
+    1760,
+    1780,
+    1800,
+    1820,
+    1840,
+    1860,
+    1880,
+    1900
+};
+/*
 const int mn[172] = {
     0,
     20,
@@ -240,7 +334,7 @@ const int mn[172] = {
     3400,
     3420,
 };
-
+*/
 const char* alphabet[62] = {
     "0", //0
     "1", //1
@@ -308,21 +402,25 @@ const char* alphabet[62] = {
 
 Mix_Music* music = NULL;
 TTF_Font* font1 = NULL;
-TTF_Font* font2 = NULL;
-TTF_Font* font3 = NULL;
-SDL_Texture* text1 = NULL;
-SDL_Surface* surface1 = NULL;
-SDL_Texture* text2 = NULL;
-SDL_Surface* surface2 = NULL;
+SDL_Texture* texthead[62] = { NULL };
+SDL_Surface* surfacehead[62] = { NULL };
+SDL_Texture* textbody[62] = { NULL };
+SDL_Surface* surfacebody[62] = { NULL };
+SDL_Texture* textempty = NULL;
+SDL_Surface* surfaceempty = NULL;
 
 int main(int argc, char* argv[])
 {
     // Initialize SDL and the relevant structures
     initialize();
 
+    // clear the screen with all black before drawing anything 
+    SDL_SetRenderDrawColor(app.renderer, 0, 0, 0, 255);
+    SDL_RenderClear(app.renderer);
+
     Mix_PlayingMusic();
 
-    float FrameTime1 = 0, FrameTime2 = 0, FrameTimeR = 0;
+    float FrameTime1 = 0, FrameTime2 = 0;
 
     int PreviousTime = 0;
     int CurrentTime = 0;
@@ -332,16 +430,13 @@ int main(int argc, char* argv[])
 
     font1 = TTF_OpenFont("matrix.ttf", 24);
 
-    int rval1 = 0;
-    int rc1 = 0;
-    int rc2 = 0;
-    int rc3 = 0;
 
-
-//    SDL_Color foreground1 = { 0, 0, 0, 255 };
-    SDL_Color background1 = { 0, 0, 0, 0 };
-    SDL_Color foreground2 = { 0, 0, 0, 0 };
-    SDL_Color background2 = { 0, 0, 0, 0 };
+    SDL_Color foregroundhead = { 255, 255, 255 };
+    SDL_Color backgroundhead = { 0, 0, 0 };
+    SDL_Color foregroundbody = { 41, 150, 23 };
+    SDL_Color backgroundbody = { 0, 0, 0 };
+    SDL_Color foregroundempty = { 0, 0, 0 };
+    SDL_Color backgroundempty = { 0, 0, 0 };
 
     // enter app loop
     while (app.running) {
@@ -356,33 +451,17 @@ int main(int argc, char* argv[])
 
         FrameTime1 += DeltaTime;
         FrameTime2 += DeltaTime;
-        FrameTimeR += DeltaTime;
 
-        // clear the screen with all black before drawing anything 
-//        SDL_SetRenderDrawColor(app.renderer, 0, 0, 0, 255);
-//        SDL_RenderClear(app.renderer);
+        surfaceempty = TTF_RenderText_LCD(font1, " ", foregroundempty, backgroundempty);
+        textempty = SDL_CreateTextureFromSurface(app.renderer, surfaceempty);
 
-//        SDL_Color foreground1 = { 144, 238, 144, 255 };
-//        SDL_Color background1 = { 0,0,0,0 };
-//        SDL_Color foreground2 = { 0, 128, 0, 255 };
-//        SDL_Color background2 = { 0,0,0,0 };
-
-        //This is here because the value needs to change and gets updated in the main loop
-        SDL_Color foreground1 = { rc1, 0, 0, 255 };
-
-        surface1 = TTF_RenderText_Shaded(font1, alphabet[rval1], foreground1, background1);
-        text1 = SDL_CreateTextureFromSurface(app.renderer, surface1);
-
-        surface2 = TTF_RenderText_Shaded(font1, " ", foreground2, background2);
-        text2 = SDL_CreateTextureFromSurface(app.renderer, surface2);
-
-        if (FrameTimeR > RFrame)
+        for (int srn = 0; srn < 62; srn++)
         {
-            rval1 = rand() % 61;
-            rc1 = (rand() % (255 - 64 + 1)) + 64;//used for red colour random (rand() % (max - min + 1)) + min;
-            rc2 = (rand() % (255 - 0 + 1)) + 0;//used for green colour random (rand() % (max - min + 1)) + min;
-            rc3 = (rand() % (255 - 0 + 1)) + 0;//used for blue colour random (rand() % (max - min + 1)) + min;
-            FrameTimeR = 0;
+            surfacehead[srn] = TTF_RenderText_LCD(font1, alphabet[srn], foregroundhead, backgroundhead);
+            texthead[srn] = SDL_CreateTextureFromSurface(app.renderer, surfacehead[srn]);
+
+            surfacebody[srn] = TTF_RenderText_LCD(font1, alphabet[srn], foregroundbody, backgroundbody);
+            textbody[srn] = SDL_CreateTextureFromSurface(app.renderer, surfacebody[srn]);
         }
 
         if (FrameTime1 > SpawnFrame)
@@ -419,11 +498,18 @@ int main(int argc, char* argv[])
 
             FrameTime2 = 0;
         }
+        
+        for (int srnclean = 0; srnclean < 62; srnclean++)
+        {
+            SDL_DestroyTexture(texthead[srnclean]);
+            SDL_FreeSurface(surfacehead[srnclean]);
 
-        SDL_DestroyTexture(text1);
-        SDL_FreeSurface(surface1);
-        SDL_DestroyTexture(text2);
-        SDL_FreeSurface(surface2);
+            SDL_DestroyTexture(textbody[srnclean]);
+            SDL_FreeSurface(surfacebody[srnclean]);
+        }
+
+        SDL_DestroyTexture(textempty);
+        SDL_FreeSurface(surfaceempty);
     }
     // make sure program cleans up on exit
     terminate(EXIT_SUCCESS);
@@ -485,7 +571,7 @@ void initialize()
 
     Mix_OpenAudio(48000, MIX_DEFAULT_FORMAT, 2, 4096);
     Mix_AllocateChannels(-1);
-    Mix_VolumeMusic(25);
+    Mix_VolumeMusic(100);
     music = Mix_LoadMUS("music.mp3");
     Mix_PlayMusic(music, -1);
 }
@@ -508,7 +594,6 @@ void terminate(int exit_code)
     Mix_Quit();
 
     TTF_CloseFont(font1);
-    TTF_CloseFont(font2);
     TTF_Quit();
 
     SDL_Quit();
@@ -530,12 +615,12 @@ void handle_input()
 
 int spawn_rain(int i)
 {
-    int RAIN_START_X = rand() % 172;
+//    int RAIN_START_X = rand() % 172; //used for 3440x1440
+    int RAIN_START_X = rand() % 96;
+    int random = rand() % 61;
 
     app.srain[i][0].x = mn[RAIN_START_X];
     app.srain[i][0].y = RAIN_START_Y;
-
-    SDL_QueryTexture(text1, NULL, NULL, &app.srain[i][0].w, &app.srain[i][0].h);
 
     for (int t = 0; t < Increment; t++)
     {
@@ -543,77 +628,39 @@ int spawn_rain(int i)
         if (t == 0)
         {
             app.srain[i][t].y = app.srain[i][t].y - 20;
+            SDL_QueryTexture(textempty, NULL, NULL, &app.srain[i][0].w, &app.srain[i][0].h); //spawn data but display nothing
         }
         else if (t == 1)
         {
-            app.srain[i][t].y = app.srain[i][t - 1].y - DM.h;
+            app.srain[i][t].y = app.srain[i][t - 1].y - 20;
+            SDL_QueryTexture(textempty, NULL, NULL, &app.srain[i][1].w, &app.srain[i][1].h); //spawn data but display nothing
         }
-
-        SDL_QueryTexture(text2, NULL, NULL, &app.srain[i][t].w, &app.srain[i][t].h);
+        else if (t == 2)
+        {
+            app.srain[i][t].y = app.srain[i][t - 2].y - DM.h;
+            SDL_QueryTexture(textempty, NULL, NULL, &app.srain[i][2].w, &app.srain[i][2].h); //spawn data but display nothing
+        }
     }
+
     return i;
 }
-
-/*Original Code
-int spawn_rain(int i)
-{
-    int RAIN_START_X = rand() % 172;
-
-    app.srain[i][0].x = mn[RAIN_START_X];
-    app.srain[i][0].y = RAIN_START_Y;
-
-    SDL_QueryTexture(text1, NULL, NULL, &app.srain[i][0].w, &app.srain[i][0].h);
-
-    for (int t = 0; t < Increment; t++)
-    {
-        app.srain[i][t].x = app.srain[i][0].x;
-        if (t == 0)
-        {
-            app.srain[i][t].y = app.srain[i][t].y - 20;
-        }
-        else if (t == 1)
-        {
-            app.srain[i][t].y = app.srain[i][t - 1].y - 2000;
-        }
-
-        SDL_QueryTexture(text2, NULL, NULL, &app.srain[i][t].w, &app.srain[i][t].h);
-    }
-    return i;
-}
-*/
 
 int move_rain(int i)
 {
-    app.srain[i][0].y = app.srain[i][0].y + app.dy;
-
-    SDL_RenderCopy(app.renderer, text1, NULL, &app.srain[i][0]);
-
-    for (int t = 1; t < Increment; t++)
-    {
-        app.srain[i][t].y = app.srain[i][t].y + app.dy;
-
-        SDL_RenderCopy(app.renderer, text2, NULL, &app.srain[i][t]);
-    }
-    return i;
-}
-
-/*Original Code
-int move_rain(int i)
-{
-//    SDL_SetRenderDrawBlendMode(app.renderer, SDL_BLENDMODE_NONE);
-//    SDL_SetRenderDrawColor(app.renderer, 255,255,255,255);
-//    SDL_RenderFillRect(app.renderer, &app.srain[i][0]);
+    int randomhead = rand() % 61;
+    int randombody = rand() % 61;
 
     app.srain[i][0].y = app.srain[i][0].y + app.dy;
 
-    SDL_RenderCopy(app.renderer, text1, NULL, &app.srain[i][0]);
+    SDL_RenderCopy(app.renderer, texthead[randomhead], NULL, &app.srain[i][0]); //Start is playing something
 
-    for (int t = 1; t < Increment; t++)
-    {
-        app.srain[i][t].y = app.srain[i][t].y + app.dy;
+    app.srain[i][1].y = app.srain[i][1].y + app.dy;
 
-        SDL_RenderCopy(app.renderer, text2, NULL, &app.srain[i][t]);
-    }
+    SDL_RenderCopy(app.renderer, textbody[randombody], NULL, &app.srain[i][1]); //Start is playing something
+
+    app.srain[i][2].y = app.srain[i][2].y + app.dy;
+
+    SDL_RenderCopy(app.renderer, textempty, NULL, &app.srain[i][2]); //Start is playing something
+
     return i;
 }
-*/
